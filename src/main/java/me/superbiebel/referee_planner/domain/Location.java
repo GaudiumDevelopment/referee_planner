90d1343633edae5e366fedfa72062f4b4109ec73
omegabiebel <superbiebel@gmail.com>
2022-05-08 16:02:10 +0200

    made the method that calculates the distance more accurate

--- src/main/java/me/superbiebel/referee_planner/domain/Location.java
+++ src/main/java/me/superbiebel/referee_planner/domain/Location.java
@@ -3,9 +3,6 @@ package me.superbiebel.referee_planner.domain;
 import lombok.Builder;
 import lombok.Getter;
 
-import static java.lang.Math.ceil;
-import static java.lang.Math.sqrt;
-
 @Builder(toBuilder = true)
 public class Location {
     public static final double METERS_PER_DEGREE = 111_000;
@@ -26,9 +23,21 @@ public class Location {
     }
     
     public long getDistanceTo(Location otherLocation) {
-        double latitudeDiff = otherLocation.latitude - this.latitude;
-        double longitudeDiff = otherLocation.longitude - this.longitude;
-        return (long) ceil(sqrt(latitudeDiff * latitudeDiff + longitudeDiff * longitudeDiff) * METERS_PER_DEGREE);
+        return Math.round((distance(latitude, longitude, otherLocation.getLatitude(), otherLocation.getLongitude())) * 1000);
+    }
+    
+    private static double distance(double lat1, double lon1, double lat2, double lon2) {
+        if ((lat1 == lat2) && (lon1 == lon2)) {
+            return 0;
+        } else {
+            double theta = lon1 - lon2;
+            double dist = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2)) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
+            dist = Math.acos(dist);
+            dist = Math.toDegrees(dist);
+            dist = dist * 60 * 1.1515;
+            dist = dist * 1.609344;
+            return (dist);
+        }
     }
     
     /**
