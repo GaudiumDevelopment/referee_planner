0cf8ebe0f71fc43d0f8072ddc10ffca487bc45a8
omegabiebel <superbiebel@gmail.com>
2022-06-10 17:28:09 +0200

    added referee pinning and updated optaplanner

--- src/main/java/me/superbiebel/referee_planner/problemchanges/gameassignment/PinRefereeToGameAssignmentChange.java
+++ src/main/java/me/superbiebel/referee_planner/problemchanges/gameassignment/PinRefereeToGameAssignmentChange.java
@@ -1,5 +1,6 @@
 package me.superbiebel.referee_planner.problemchanges.gameassignment;
 
+import lombok.Builder;
 import lombok.Getter;
 import me.superbiebel.referee_planner.domain.GameAssignment;
 import me.superbiebel.referee_planner.domain.Referee;
@@ -9,6 +10,7 @@ import org.optaplanner.core.api.solver.change.ProblemChangeDirector;
 
 import java.util.UUID;
 
+@Builder(toBuilder = true)
 public class PinRefereeToGameAssignmentChange implements ProblemChange<RefereeTimeTable> {
     @Getter
     private UUID refereeUUID;
@@ -20,5 +22,6 @@ public class PinRefereeToGameAssignmentChange implements ProblemChange<RefereeTi
         Referee referee = problemChangeDirector.lookUpWorkingObject(Referee.builder().refereeUUID(refereeUUID).build()).orElseThrow();
         GameAssignment gameAssignment = problemChangeDirector.lookUpWorkingObject(GameAssignment.builder().assignmentUUID(assignmentUUID).build()).orElseThrow();
         problemChangeDirector.changeVariable(gameAssignment, "referee", gameAssignment1 -> gameAssignment1.setReferee(referee));
+        problemChangeDirector.changeProblemProperty(referee, referee1 -> referee1.addAssignment(gameAssignment));
     }
 }
--- src/test/java/me/superbiebel/tests/problemchange/GameAssignmentProblemChangeTests.java
+++ src/test/java/me/superbiebel/tests/problemchange/GameAssignmentProblemChangeTests.java
@@ -1,7 +1,42 @@
 package me.superbiebel.tests.problemchange;
 
 import io.quarkus.test.junit.QuarkusTest;
+import me.superbiebel.referee_planner.domain.Referee;
+import me.superbiebel.referee_planner.domain.RefereeTimeTable;
+import me.superbiebel.referee_planner.domain.data.TimeTableGenerator;
+import me.superbiebel.referee_planner.problemchanges.gameassignment.PinRefereeToGameAssignmentChange;
+import org.junit.jupiter.api.*;
+import org.optaplanner.core.api.score.ScoreManager;
+import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore;
+import org.optaplanner.core.api.solver.SolverManager;
+
+import javax.inject.Inject;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @QuarkusTest
-public class GameAssignmentProblemChangeTests {
+class GameAssignmentProblemChangeTests {
+    
+    @Inject
+    SolverManager<RefereeTimeTable, Long> solverManager;
+    @Inject
+    ScoreManager<RefereeTimeTable, HardSoftScore> scoreManager;
+    
+    @Test
+    @Timeout(unit = TimeUnit.MINUTES, value = 2)
+    void pinRefereeChangeTest() throws InterruptedException {
+        TimeTableGenerator timeTableGenerator = new TimeTableGenerator().amountOfGames(300).amountOfReferees(900);
+        RefereeTimeTable intermediateTimeTable = timeTableGenerator.build();
+        UUID refereeUUID = intermediateTimeTable.getReferees().get(0).getRefereeUUID();
+        UUID gameAssignmentUUID = intermediateTimeTable.getGameAssignments().get(0).getAssignmentUUID();
+    
+        RefereeTimeTable refereeTimeTable = new ProblemChangeSolver().refereeTimeTableProblemChangeSolver(solverManager, scoreManager, "local/solverOutput/availabilityProblemChangeTests",
+                PinRefereeToGameAssignmentChange.builder().assignmentUUID(gameAssignmentUUID).refereeUUID(refereeUUID).build(),
+                intermediateTimeTable.toBuilder()
+                        .build(), timeTableGenerator);
+        Referee adaptedReferee = refereeTimeTable.getReferees().stream().filter(referee1 -> referee1.getRefereeUUID().equals(refereeUUID)).findFirst().orElseThrow();
+        assertTrue(adaptedReferee.getAssignments().stream().anyMatch(assignment -> assignment.getAssignmentUUID().equals(gameAssignmentUUID)));
+    }
 }
